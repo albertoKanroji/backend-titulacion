0ffca59de164c3700e7147059f6bb053f39462fe
albertoKanroji <albert.hdz855@gmail.com>
2024-05-11 21:45:04 -0600

    cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -13,22 +13,8 @@ class CorsMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
-        header("Access-Control-Allow-Origin: *");
-        //ALLOW OPTIONS METHOD
-        $headers = [
-            'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
-        ];
-        if ($request->getMethod() == "OPTIONS") {
-            //The client-side application can set only headers allowed in Access-Control-Allow-Headers
-                return response()->json('OK',200,$headers);
-            }
-            $response = $next($request);
-            foreach ($headers as $key => $value) {
-                $response->header($key, $value);
-            }
-            return $response;
-        }
+        return $next($request);
+    }
 }
