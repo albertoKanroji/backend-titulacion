bc7bcae8f04db58ad8eecd93ba22921d26a9f21c
albertoKanroji <albert.hdz855@gmail.com>
2024-05-11 22:31:14 -0600

    cors

--- routes/api.php
+++ routes/api.php
@@ -19,33 +19,31 @@ use App\Http\Controllers\GruposMuscularesControllerlAPI;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['middleware' => ['cors']], function () {
+    Route::prefix('v1')->group(function () {
 
-Route::prefix('v1')->group(function () {
+        Route::prefix('usuarios')->group(function () {
 
-    Route::prefix('usuarios')->group(function () {
+            //  Route::get('/', [CustomersController::class, 'index'])->name('usuarios.index');
+            Route::post('/login', [CustomersController::class, 'login'])->name('usuarios.login');
+            Route::post('/', [CustomersController::class, 'store'])->name('usuarios.store');
+            // Route::get('/{id}', [CustomersController::class, 'show'])->name('usuarios.show');
+            // Route::put('/{id}', [CustomersController::class, 'update'])->name('usuarios.update');
+            // Route::delete('/{id}', [CustomersController::class, 'destroy'])->name('usuarios.destroy');
+        });
 
-        //  Route::get('/', [CustomersController::class, 'index'])->name('usuarios.index');
-        Route::post('/login', [CustomersController::class, 'login'])->name('usuarios.login');
-        Route::post('/', [CustomersController::class, 'store'])->name('usuarios.store');
-        // Route::get('/{id}', [CustomersController::class, 'show'])->name('usuarios.show');
-        // Route::put('/{id}', [CustomersController::class, 'update'])->name('usuarios.update');
-        // Route::delete('/{id}', [CustomersController::class, 'destroy'])->name('usuarios.destroy');
+        Route::prefix('rutinas')->group(function () {
+
+            Route::get('/', [RutinasControllerAPI::class, 'index']);
+            Route::get('/{id}', [RutinasControllerAPI::class, 'show']);
+            Route::get('/{id}/ejercicios', [RutinasControllerAPI::class, 'showEjercicios']);
+        });
+        Route::prefix('grupos-musculares')->group(function () {
+
+            Route::get('/', [GruposMuscularesControllerlAPI::class, 'index']);
+            Route::get('/{id}', [GruposMuscularesControllerlAPI::class, 'show']);
+            Route::get('/{id}/video', [GruposMuscularesControllerlAPI::class, 'showVideos']);
+            Route::get('/video/{id}', [GruposMuscularesControllerlAPI::class, 'showVideoDetail']);
+        });
     });
-
-    Route::prefix('rutinas')->group(function () {
-
-        Route::get('/', [RutinasControllerAPI::class, 'index']);
-        Route::get('/{id}', [RutinasControllerAPI::class, 'show']);
-        Route::get('/{id}/ejercicios', [RutinasControllerAPI::class, 'showEjercicios']);
-    
-    });
-    Route::prefix('grupos-musculares')->group(function () {
-
-        Route::get('/', [GruposMuscularesControllerlAPI::class, 'index']);
-         Route::get('/{id}', [GruposMuscularesControllerlAPI::class, 'show']);
-        Route::get('/{id}/video', [GruposMuscularesControllerlAPI::class, 'showVideos']);
-        Route::get('/video/{id}', [GruposMuscularesControllerlAPI::class, 'showVideoDetail']);
-    });
-
-
 });
